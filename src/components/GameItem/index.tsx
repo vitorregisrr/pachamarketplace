ca5608b097225babe4508eafea5b0a0f9cc965ae
vitorregisr <vitorregisrr@gmail.com>
2023-01-20 14:17:09 -0300

    Add GameItem

---
+++ src/components/GameItem/index.tsx
@@ -0,0 +1,60 @@
+import { Download } from '@styled-icons/remix-fill'
+
+import * as S from './styles'
+
+export type PaymentInfoProps = {
+  number: string
+  flag: string
+  img: string
+  purchaseDate: string
+}
+
+export type GameItemProps = {
+  img: string
+  title: string
+  price: string
+  downloadLink?: string
+  paymentInfo?: PaymentInfoProps
+}
+
+const GameItem = ({
+  img,
+  title,
+  price,
+  downloadLink,
+  paymentInfo
+}: GameItemProps) => (
+  <S.Wrapper>
+    <S.GameContent>
+      <S.ImageBox>
+        <img src={img} alt={title} />
+      </S.ImageBox>
+
+      <S.Content>
+        <S.Title>{title}</S.Title>
+        {!!downloadLink && (
+          <S.DownloadLink
+            href={downloadLink}
+            target="_blank"
+            aria-label={`Get ${title} here`}
+          >
+            <Download size={22} />
+          </S.DownloadLink>
+        )}
+        <S.Price>{price}</S.Price>
+      </S.Content>
+    </S.GameContent>
+
+    {!!paymentInfo && (
+      <S.PaymentContent>
+        <p>{paymentInfo.purchaseDate}</p>
+        <S.CardInfo>
+          <span>{paymentInfo.number}</span>
+          <img src={paymentInfo.img} alt={paymentInfo.flag} />
+        </S.CardInfo>
+      </S.PaymentContent>
+    )}
+  </S.Wrapper>
+)
+
+export default GameItem
